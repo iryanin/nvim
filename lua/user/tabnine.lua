90e724997e1e8ba4869b5d5ff4cc7e62d082ca7c
iryandyl <iryandyl@gmail.com>
2022-10-18 13:43:20 +0800

    tabnine improvement

---
+++ lua/user/tabnine.lua
@@ -0,0 +1,24 @@
+local tabnine = require('cmp_tabnine.config')
+
+tabnine.setup({
+	max_lines = 1000,
+	max_num_results = 20,
+	sort = true,
+	run_on_every_keystroke = true,
+	snippet_placeholder = '..',
+	ignored_file_types = { 
+		-- default is not to ignore
+		-- uncomment to ignore in lua:
+		-- lua = true
+	},
+	show_prediction_strength = false
+})
+local prefetch = vim.api.nvim_create_augroup("prefetch", { clear = true })
+
+vim.api.nvim_create_autocmd("BufRead", {
+	group = prefetch,
+	pattern = "*.py",
+	callback = function()
+		require("cmp_tabnine"):prefetch(vim.fn.expand("%:p"))
+	end,
+})
