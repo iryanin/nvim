8bd546aa842e22b2a44abe2edffdd49968972fab
iryandyl <221250063@smail.nju.edu.cn>
2024-03-16 21:42:46 +0800

    remove useless loc in nvim-tree

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -77,11 +77,6 @@ require("lazy").setup({
         "kyazdani42/nvim-tree.lua",
         config = function()
             require("nvim-tree").setup({
-                actions = {
-                    open_file = {
-                        quit_on_open = true,
-                    },
-                },
             })
         end,
     },
