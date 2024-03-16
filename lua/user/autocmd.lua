416235885b2a13ce2e2620556ecb0c2853033f41
iryandyl <221250063@smail.nju.edu.cn>
2024-03-16 22:04:35 +0800

    bugfix: remove useless autocmd

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -29,15 +29,3 @@ vim.api.nvim_create_autocmd("FileType", {
     group = brpGrp,
     command = ":lua require('user.util').load_breakpoints()",
 })
-
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-    pattern = 'NvimTree*',
-    callback = function()
-        local api = require('nvim-tree.api')
-        local view = require('nvim-tree.view')
-
-        if not view.is_visible() then
-            api.tree.open()
-        end
-    end,
-})
